ac84d43033a56f3e0471f1790860f7391c463390
Mike <mcmiller905@gmail.com>
2021-01-01 13:28:35 -0500

    added days 5 to 10

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Advent Of Code 2020 in Python
 # Code by mcmiller905@gmail.com
 # Change dayToRun to the desired Day
-dayToRun = 1
+dayToRun = 10
 
 # Run desired day's code
 if dayToRun == 1:
@@ -32,3 +32,45 @@ if dayToRun == 4:
     print()
     import DayCode.day4 as day4
     day4.runCode()
+if dayToRun == 5:
+    print("#####################")
+    print("####### DAY 5 #######")
+    print("#####################")
+    print()
+    import DayCode.day5 as day5
+    day5.runCode()
+if dayToRun == 6:
+    print("#####################")
+    print("####### DAY 6 #######")
+    print("#####################")
+    print()
+    import DayCode.day6 as day6
+    day6.runCode()
+if dayToRun == 7:
+    print("#####################")
+    print("####### DAY 7 #######")
+    print("#####################")
+    print()
+    import DayCode.day7 as day7
+    day7.runCode()
+if dayToRun == 8:
+    print("#####################")
+    print("####### DAY 8 #######")
+    print("#####################")
+    print()
+    import DayCode.day8 as day8
+    day8.runCode()
+if dayToRun == 9:
+    print("#####################")
+    print("####### DAY 9 #######")
+    print("#####################")
+    print()
+    import DayCode.day9 as day9
+    day9.runCode()
+if dayToRun == 10:
+    print("#####################")
+    print("####### DAY 10 ######")
+    print("#####################")
+    print()
+    import DayCode.day10 as day10
+    day10.runCode()
