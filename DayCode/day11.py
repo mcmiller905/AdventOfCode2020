5d0eafcd6a002f6b2033022c21f066e20f49d4e0
Mike <mcmiller905@gmail.com>
2021-01-19 11:15:14 -0500

    some formatting to prep for day10 p2

--- DayCode/day11.py
+++ DayCode/day11.py
@@ -4,7 +4,7 @@ def runCode():
 
     grid = lines
     print("START")
-    printGrid(grid)
+    #printGrid(grid)
     for iter in range(300):
         newGrid = []
         for i in range(len(grid)):
@@ -13,13 +13,19 @@ def runCode():
             for j in range(len(row)):
                 seat = row[j]
                 if seat == "L":
-                    numAdj = getAdjSeats(i,j,grid)
+                    #part 1
+                    #numAdj = getAdjSeats(i,j,grid)
+                    #part 2
+                    numAdj = getVisibleSeats(i,j,grid)
                     if(numAdj == 0):
                         newRow += "#"
                     else:
                         newRow += "L"
                 elif seat == "#":
-                    numAdj = getAdjSeats(i,j,grid)
+                    #part 1
+                    #numAdj = getAdjSeats(i,j,grid)
+                    #part 2
+                    numAdj = getVisibleSeats(i,j,grid)
                     if(numAdj > 3):
                         newRow += "L"
                     else:
@@ -28,15 +34,53 @@ def runCode():
                     newRow += "."
             newGrid.append(newRow)
         if grid == newGrid:
+            printGrid(grid)
             print("this is it")
             print("total sitters: " + str(countSitters(grid)))
             break
         grid = newGrid
         print(str(iter) + "ITER")
-        printGrid(grid)
+        #printGrid(grid)
 
 
 def getAdjSeats(i,j,grid):
+    #part 1
+    numAdj = 0
+    # check line before
+    if(i-1>=0):
+        preLine = grid[i-1]
+        if(j-1>=0):
+            if(preLine[j-1] == "#"):
+                numAdj += 1
+        if(preLine[j] == "#"):
+            numAdj += 1
+        if(j+1 < len(preLine)):
+            if(preLine[j+1] == "#"):
+                numAdj += 1
+    # check same line
+    curLine = grid[i]
+    if(j-1>=0):
+        if(curLine[j-1] == "#"):
+            numAdj += 1
+    if(j+1<len(curLine)):
+        if(curLine[j+1] == "#"):
+            numAdj += 1
+    # check next line
+    if(i+1<len(grid)):
+        nextLine = grid[i+1]
+        if(j-1>=0):
+            if(nextLine[j-1] == "#"):
+                numAdj += 1
+        if(nextLine[j] == "#"):
+            numAdj += 1
+        if(j+1 < len(nextLine)):
+            if(nextLine[j+1] == "#"):
+                numAdj += 1
+    return numAdj
+
+
+def getVisibleSeats(i,j,grid):
+    #part 2
     numAdj = 0
     # check line before
     if(i-1>=0):
--- readme.md
+++ readme.md
@@ -2,5 +2,5 @@
 ### Code by mcmiller905
 
 This project contains (_or will contain_) the Python solutions for
-the 2020 Advent of Code event. A variable in **main.py** changes which
+the 2020 Advent of Code event. The variable _dayToRun_ in **main.py** changes which
 day's code is run.
